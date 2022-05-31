e34cdeb734d36a289b62f9d9a6b845c30d4cecef
Rushikesh <rushi96bhosale@gmail.com>
2022-05-31 18:44:23 +0530

    This is first commit

---
+++ src/abc/Abc.java
@@ -0,0 +1,16 @@
+package abc;
+
+public class Abc {
+	System.out.println("This is abc method");
+}
+public void pqr{
+	system.out.println("this is pqr method");
+	
+
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
